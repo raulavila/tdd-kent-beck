780b15ac5aadf862675690d9a40eb2af79353464
RaulAvila <ravila80@gmail.com>
2016-01-11 19:29:28 +0000

    Missed annotations

--- src/main/java/com/raulavila/money/Money.java
+++ src/main/java/com/raulavila/money/Money.java
@@ -38,6 +38,7 @@ public class Money implements Expression {
         return amount + " " + currency;
     }
 
+    @Override
     public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
